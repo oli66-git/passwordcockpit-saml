06f61c4f76e096ef202ef1d758990e5ade0f8e54
olccc <o.cayre@waroude.com>
2026-01-28 09:07:05 +0100

    Fix: enable output buffering and redirect entrypoint output to stderr

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,12 +14,12 @@ fi
 
 # install passwordcockpit only if the volume is empty
 if [ $count -gt "2" ]; then
-        echo -e "\e[31mWARNING: $PWD is not empty! Passwordcockpit will not be installed\e[0m"
+        echo -e "\e[31mWARNING: $PWD is not empty! Passwordcockpit will not be installed\e[0m" >&2
 else
     # move passwordcockpit source in container
     shopt -s dotglob
     mv /usr/src/passwordcockpit/* ./
-    echo -e "\e[32mSource copied in $PWD\e[0m"
+    echo -e "\e[32mSource copied in $PWD\e[0m" >&2
 
     ##############################################
     # Configuration files
@@ -37,12 +37,12 @@ else
     
     sed -ri -e 's!PASSWORDCOCKPIT_BASEHOST!'${PASSWORDCOCKPIT_SWAGGER_API_HOST}'!g' swagger/swagger.json
     
-    echo -e "\e[32mConfiguration files created and modified\e[0m"
+    echo -e "\e[32mConfiguration files created and modified\e[0m" >&2
 
     ##############################################
     # Database
     ##############################################
-    echo -e "\e[31mCheck database connection\e[0m"
+    echo -e "\e[31mCheck database connection\e[0m" >&2
     max_retries=10
     try=0
 
@@ -51,7 +51,7 @@ else
         connection=$(vendor/bin/doctrine dbal:run-sql "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${PASSWORDCOCKPIT_DATABASE_DATABASE}'")
         # schema_name is unset or set to the empty string so connecting problem
         if [ -z "${connection}" ]; then
-            echo -e "\e[31mRetrying connection...\e[0m"
+            echo -e "\e[31mRetrying connection...\e[0m" >&2
             try=$((try+1))
             sleep 3s
             continue
@@ -59,29 +59,29 @@ else
         schema_exist=$(echo $connection | grep ${PASSWORDCOCKPIT_DATABASE_DATABASE} -c)
         # connection ok and schema exist
         if [ "$schema_exist" == "1" ]; then
-            echo -e "\e[32mConnection ok\e[0m"
-            echo -e "\e[32mSchema already exist\e[0m"
+            echo -e "\e[32mConnection ok\e[0m" >&2
+            echo -e "\e[32mSchema already exist\e[0m" >&2
             # Tables exists
             number_of_tables=$(vendor/bin/doctrine dbal:run-sql "SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '${PASSWORDCOCKPIT_DATABASE_DATABASE}'" | tr -d -c 0-9)
             if [ "$number_of_tables" == "0" ]; then
                 # Create the tables and popolate it
                 vendor/bin/doctrine orm:schema-tool:create
                 vendor/bin/doctrine orm:generate-proxies
-                echo -e "\e[32mDatabase created\e[0m"
+                echo -e "\e[32mDatabase created\e[0m" >&2
                 sql=$(cat database/create-tests-environment.sql | sed '/^--/d')
                 vendor/bin/doctrine dbal:run-sql "$sql"
-                echo -e "\e[32mTest data installed\e[0m"
+                echo -e "\e[32mTest data installed\e[0m" >&2
             else
                 # Update scripts
-                echo -e "\e[32mNo updates to be carried out\e[0m"
+                echo -e "\e[32mNo updates to be carried out\e[0m" >&2
             fi
             break
         fi
         # connection ok and schema not exist: error
         if [ "$schema_exist" == "0" ]; then
-            echo -e "\e[32mConnection ok\e[0m"
-            echo -e "\e[31mSchema not exist\e[0m"
-            echo -e "\e[31mInstalling failed!\e[0m"
+            echo -e "\e[32mConnection ok\e[0m" >&2
+            echo -e "\e[31mSchema not exist\e[0m" >&2
+            echo -e "\e[31mInstalling failed!\e[0m" >&2
             exit 1
             break
         fi
@@ -92,7 +92,7 @@ else
         exit 1
     fi
 
-    echo -e "\e[32mPasswordcockpit ready\e[0m"
+    echo -e "\e[32mPasswordcockpit ready\e[0m" >&2
     
 fi
 
